d3d5575ec943a6783e745a1c810b90cf6e13b686
sangtrants25 <1112250.tsang@gmail.com>
2017-03-05 23:58:12 +0700

    first commit

---
+++ src/main/java/com/erp/SpringAbcApplication.java
@@ -0,0 +1,12 @@
+package com.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAbcApplication.class, args);
+	}
+}
